23af25346e420a1a5cb488c6a926e6328a74c8b3
Chan <vicg0923@gmail.com>
2025-10-24 11:42:33 +0900

    초기 커밋

---
+++ src/main/java/com/example/spring_friends/SpringFriendsApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_friends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFriendsApplication.class, args);
+	}
+
+}
